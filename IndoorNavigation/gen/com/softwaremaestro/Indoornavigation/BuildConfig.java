af34d05c9cce7c6f4baae21953d19cff323b9c51
junsung <limjunsung@gmail.com>
2012-11-21 18:48:05 +0900

    Revert "data"
    This reverts commit 0e4a729ebe582c6dc9dba1db8b7e62e1090af5cc.

--- IndoorNavigation/gen/com/softwaremaestro/Indoornavigation/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.softwaremaestro.Indoornavigation;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Indoormap/gen/com/softwaremaestro/indoormap/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.softwaremaestro.indoormap;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
